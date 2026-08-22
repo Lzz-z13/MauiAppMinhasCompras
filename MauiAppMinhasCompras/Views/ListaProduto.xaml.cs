f676ccd3cdd927e2b6d4147d2d85ea7da4b86865
Luiz Felipe Campos Margato <luizfelipe.camposmargato@gmail.com>
2026-08-22 14:49:25 -0300

    feat(Page: ListaProduto): creating new Page for viewing all the products registered in the DB, besides the search bar :)

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,10 +1,24 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
+	}
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
 	}
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -16,5 +30,35 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlert("ops", ex.Message, "ok");
 		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+		DisplayAlert("Total dos Produtos", msg, "OK!");	
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        MenuItem menuItem = (MenuItem)sender;
+
+        Produto produto = (Produto)menuItem.BindingContext;
+
+        await App.Db.Delete(produto.Id);
+
+        lista.Remove(produto);
     }
 }
